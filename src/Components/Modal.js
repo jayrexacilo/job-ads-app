1751610618c57d998a102c0b755b914a9ea8f820
jayrexacilo <acilojayrex@gmail.com>
2017-11-07 23:18:24 +0800

    Added a handler for submitting the form and simple validation

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,6 +1,46 @@
 import React, { Component } from 'react';
 
 export default class Modal extends Component {
+    constructor() {
+        super();
+        this.state = {
+            username: '',
+            password: '',
+            loginType: ''
+        };
+    }
+    handleLogin(e) {
+        const username = e.target.username.value;
+        const password = e.target.password.value;
+        const loginType = e.target.loginType.value;
+        let loginTypeValid = false;
+        
+        switch(loginType) {
+            case 'employer':
+                loginTypeValid = true;
+                break;
+            case 'applicant':
+                loginTypeValid = true;
+                break;
+            default:
+                break;
+        }
+        
+        if(!loginTypeValid) {
+            alert('Something is wrong, please reload and try again.');
+        } else {
+            this.setState({
+                username: username,
+                password: password,
+                loginType: loginType
+            }, function() {
+                // send request to REST API login
+                console.log(this.state);
+            });
+        }
+        
+        e.preventDefault();
+    }
     render() {
         return (
             <div className="Modal">
@@ -15,18 +55,19 @@ export default class Modal extends Component {
                                 </button>
                             </div>
                             <div className="modal-body">
-                                <form>
+                                <form onSubmit={this.handleLogin.bind(this)}>
                                     <div className="form-group">
-                                        <label htmlFor="username" className="col-form-label">Username:</label>
-                                        <input type="text" className="form-control" id="username" />
+                                        <label htmlFor="username" className="col-form-label" >Username:</label>
+                                        <input type="text" className="form-control" id="username" name="username" required />
                                     </div>
                                     <div className="form-group">
                                         <label htmlFor="password" className="col-form-label">Password:</label>
-                                        <input type="password" className="form-control" id="password" />
+                                        <input type="password" className="form-control" id="password" name="password" required />
                                     </div>
                                     <div className="modal-footer">
+                                        <input type="hidden" value="employer" name="loginType" required />
                                         <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                                        <button type="submit" className="btn btn-primary">Login</button>
+                                        <input type="submit" className="btn btn-primary" value="Login" />
                                     </div>
                                 </form>
                             </div>
@@ -44,18 +85,19 @@ export default class Modal extends Component {
                                 </button>
                             </div>
                             <div className="modal-body">
-                                <form>
+                                <form onSubmit={this.handleLogin.bind(this)}>
                                     <div className="form-group">
-                                        <label htmlFor="username" className="col-form-label">Username:</label>
-                                        <input type="text" className="form-control" id="username" />
+                                        <label htmlFor="username" className="col-form-label" name="username">Username:</label>
+                                        <input type="text" className="form-control" id="username" required />
                                     </div>
                                     <div className="form-group">
                                         <label htmlFor="password" className="col-form-label">Password:</label>
-                                        <input type="password" className="form-control" id="password" />
+                                        <input type="password" className="form-control" id="password" name="password" required />
                                     </div>
                                     <div className="modal-footer">
+                                        <input type="hidden" value="applicant" name="loginType" required />
                                         <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                                        <button type="submit" className="btn btn-primary">Login</button>
+                                        <input type="submit" className="btn btn-primary" value="Login" />
                                     </div>
                                 </form>
                             </div>
